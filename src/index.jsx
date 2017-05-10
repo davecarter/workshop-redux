05b7dbceaafe4e9448778bdb22fcf56142aa9fe9
David G <david.garcia@scmspain.com>
2017-05-10 18:27:45 +0200

    adds reducer function and returns initial state

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import counter from './reducer/index.jsx'
+
+const store = createStore(counter)
+const accent = {color: '#F00'}
 
 export default class App extends Component {
+  getState () {
+    return store.getState()
+  }
+
   render () {
     return (
-      <h1>Testing redux</h1>
+      <div>
+        <h1>Current Store State: <span style={accent}>{this.getState()}</span></h1>
+      </div>
     )
   }
 }
