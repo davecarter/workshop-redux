8f88d7617b7711e875007c9dbb98e2f5a788aea8
David G <david.garcia@scmspain.com>
2017-11-10 10:42:49 +0100

    adds REDUX EXTENSION

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,6 +1,9 @@
 import { createStore } from 'redux'
 import counter from '../reducer/index.jsx'
 
-const store = createStore(counter)
+const store = createStore(
+  counter,
+     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store
